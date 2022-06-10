c084125566f3d8d563606b473c6529b5ec23f4c0
heejaykong <heejay.dev@gmail.com>
2022-06-10 12:24:59 +0900

    chore: initialize

---
+++ src/main/java/com/mycompany/backend/FinalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBackEndApplication.class, args);
+	}
+
+}
